e25e2a8306576172352a095822f165e1a63a3e11
Abner Lima <abnerlima.1997@gmail.com>
2023-01-19 23:40:48 -0300

    wip:linked-list refactor

--- src/linked-list/index.ts
+++ src/linked-list/index.ts
@@ -1,59 +1,66 @@
 import { defaultEquals } from "../utils";
 
 interface _Node<T> {
-    element: T;
-    next: _Node<T> | null;
+  element: T;
+  next: _Node<T> | null;
 }
 
 interface LinkedList<T> {
-    push: (element: T) => LinkedList<T>;
-    insert: (element: T, index: number) => LinkedList<T>;
-    getElementAt: (index: number) => T | undefined;
-    remove: (element: T) => LinkedList<T>;
-    indexOf: (element: T) => number;
-    removeAt: (index: number) => LinkedList<T>;
-    isEmpty: () => boolean;
-    size: () => number;
-    toString: () => string;
+  push: (element: T) => LinkedList<T>;
+  insert: (element: T, index: number) => LinkedList<T>;
+  getElementAt: (index: number) => T | undefined;
+  remove: (element: T) => LinkedList<T>;
+  indexOf: (element: T) => number;
+  removeAt: (index: number) => LinkedList<T>;
+  isEmpty: () => boolean;
+  size: () => number;
+  toString: () => string;
 }
+
+const createNode = <T>(element: T) => ({ element, next: null });
+
+const getLastNode = <T>(head: _Node<T> | null) => {
+  let current = head;
+
+  if (current === null) return;
+
+  while (current.next) {
+    current = current.next;
+  }
+
+  return current;
+};
 
 const createLinkedList = <T>(equalsFn = defaultEquals) => {
-    let head: _Node<T> | null = null;
-    const setHead = (newHead: _Node<T> | null) => head = newHead;
+  let head: _Node<T> | null = null;
+  const setHead = (newHead: _Node<T> | null) => (head = newHead);
 
-    let length = 0;
-    const setLength = (newLength: number) => length = newLength;
+  let length = 0;
+  const setLength = (newLength: number) => (length = newLength);
 
-    function linkedList(equalsFn = defaultEquals) {
+  const linkedList = (equalsFn = defaultEquals) => {
+    const push = (element: T) => {
+      setLength(length + 1);
 
-        const push = (element: T) => {
-            setLength(length + 1);
+      const node = createNode(element);
+      const lastNode = getLastNode(head);
 
-            const node = { element, next: null };
+      if (!lastNode) {
+        setHead(node);
+      } else {
+        lastNode.next = node;
+      }
 
-            if (!head) {
-                setHead(node);
-                return linkedList(equalsFn);
-            }
-
-            let current = head;
-
-            while (current.next) {
-                current = current.next;
-            }
-
-            current.next = node;
-
-            return linkedList(equalsFn);
-        }
-
-        return {
-            push,
-            length
-        }
+      return linkedList(equalsFn);
     };
 
-    return linkedList(equalsFn);
-}
+    return {
+      push,
+      length,
+    };
+  };
+
+  return linkedList(equalsFn);
+};
 
 export default createLinkedList;
